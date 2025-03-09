913bc3086fc51a6552fdd75523c72cca70880e19
denliehoo <denliehoo@gmail.com>
2025-03-09 11:20:09 +0800

    Fix hydration issue with CSS SSR due to styled-components

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   devIndicators: false,
+  // https://nextjs.org/docs/architecture/nextjs-compiler#styled-components
+  compiler: { styledComponents: true },
 };
 
 export default nextConfig;
